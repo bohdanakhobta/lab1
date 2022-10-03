18b6da8ce1835d9a247aef5facfa373c53134df5
bhobt <bhobta15@gmail.com>
2022-10-03 15:43:23 +0300

    lab1

---
+++ Fibka/Fibula.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{
+    int n1 = 1, n2 = 1;
+    int high;
+    cout << "Amount of numbers:" << endl;
+    cin >> high;
+
+    while (n2 <= high) {
+        cout << n2 << endl;
+        int temp = n1;
+        n1 = n2;
+        n2 = n1 + temp;
+    }
+    return 0;
+}
