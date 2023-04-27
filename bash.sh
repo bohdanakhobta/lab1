9345000986f23083f544ca3cb943d2b6745d2f50
bohdanakhobta <114878770+bohdanakhobta@users.noreply.github.com>
2023-04-27 22:23:27 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/grid/testbed/tb117/vec_samples/src
+cd /home/grid/testbed/tb263/vec_samples/src
 ml icc
 arr=( "fpu" "vme" "de" "pse" "tsc" "msr" "pae" "mce" "cx8" "apic" "sep" "mtrr" "pge" "mca" "cmov" "pat" "pse36" "clflush" "dts" "acpi" "mmx" "fxsr" "sse" "sse2" "ss" "ht" "tm" "pbe" "syscall" "nx" "pdpe1gb" "rdtscp" "lm" "constant_t" "sc" "arch_perfmon" "pebs" "bts" "rep_good" "nopl" "xtopology" "nonstop_tsc" "aperfmperf" "eagerfpu" "pni" "pclmulqdq" "dtes64" "monitor" "ds_cpl" "vmx" "smx" "est" "tm2" "ssse3" "cx16" "xtpr" "pdcm" "pcid" "dca" "sse4_1" "sse4_2" "x2apic" "popcnt" "tsc_deadline_timer" "aes" "xsave" "avx" "lahf_lm" "epb" "ssbd" "ibrs" "ibpb" "stibp" "tpr_shadow" "vnmi" "flexpriority" "ept" "vpid" "xsaveopt" "dtherm" "ida" "arat" "pln" "pts" "md_clear" "spec_ctrl" "intel_stibp" "flush_l1d")
 for j in "${arr[@]}"; do
